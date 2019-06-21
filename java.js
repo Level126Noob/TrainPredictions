c7cf303521051718743b4553394a408a8b37aba7
travis <abe.travis@outlook.com>
2019-06-20 18:23:45 -0700

    attempting to authenticate

--- java.js
+++ java.js
@@ -16,6 +16,27 @@ firebase.database.enableLogging(function(message) {
     console.log("[FIREBASE]", message);
   });
 
+  firebase.auth().signInAnonymously().catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+    // ...
+  });
+
+
 var database = firebase.database();
 //=====================================================================================================================================================================================
 
